b26036b39bf24c296b50070ee0227c91bf95ebda
MadhuBabu138 <madhutechskills09@gmail.com>
2024-10-04 21:11:39 +0530

    commit2

--- MultipleInher.py
+++ MultipleInher.py
@@ -1,12 +1,20 @@
 class Base1:
-    def __init__(self):
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
         print("Base1 class constructor..")
+
+    def display1(self):
+        print("a:\t",self.a)
+        print("a:\t",self.b)
+
 class Base2:
-    def display(self):
-        print("Base display...")
+    def display2(self):
+        print("Base2 display2...")
 
 class Child(Base1,Base2):
     pass
 
-c1=Child()
-c1.display()
+c1=Child(100,200)
+c1.display1()
+c1.display2()
